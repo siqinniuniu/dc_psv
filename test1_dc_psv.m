0c8bf2b9f0d15bc58e79c7920c58f136bdc26141
taotaokai <taotaokai@gmail.com>
2014-04-01 14:30:56 +0800

    data of station on ice sheet

---
+++ test1_dc_psv.m
@@ -0,0 +1,51 @@
+%make synthetic plane P-wave response in plane layered elastic model and
+%downward continuate
+
+clear all;close all;clc
+%% parameters
+
+%time samples
+t0 = -10;
+t1 = 30;
+fs = 20;
+
+%Earth model
+vp = [6.4 8.1]; % km/s
+vs = [3.65 4.5]; % km/s
+rho = [2.7 3.3]; % g/cm^3
+thik = [35 0];  % km
+nlyr = 2;
+
+%incident wave
+rayp = 0.06; % s/km
+
+%low-pass filter for making receiver function
+a = 5;
+
+%% SACST_synPRF_haskell
+
+[sacst,t] = SACST_synPRF_haskell(nlyr,vp,vs,rho,thik,t0,t1,fs,rayp,a);
+
+vr = sacst(1).data;
+vz = sacst(2).data;
+rz = sacst(3).data;
+
+%% downward continuate the surface record
+
+nt = length(t);
+v0 = zeros(4,nt);
+v0(1,:) = vr;
+v0(2,:) = -vz;
+
+m1 = dc_psv(...
+    nlyr,vp,vs,rho,thik,... % earth model
+    nt,v0,fs,...            % time samples of velocity-stress vector
+    rayp);                  % ray parameter
+
+%% plot
+
+figure
+subplot(3,1,1); plot(t,vr,'k',t,vz,'r',t,rz,'b'); legend('vr','vz','rz')
+subplot(3,1,2); plot(t,m1(1,:),t,m1(2,:)); legend('Pd','Pu')
+subplot(3,1,3); plot(t,m1(3,:),t,m1(4,:)); legend('Sd','Su')
+xlabel('Time (s)')
--- test2_ice_bedrock.m
+++ test2_ice_bedrock.m
@@ -1,5 +1,5 @@
-%make synthetic plane P-wave response in plane layered elastic model and
-%downward continuate
+%downward continuate surface plane wavefield by a station on top of the
+%ice sheet.
 
 clear all;close all;clc
 %% parameters
@@ -9,11 +9,11 @@ t0 = -10;
 t1 = 30;
 fs = 20;
 
-%Earth model
-vp = [6.4 8.1]; % km/s
-vs = [3.65 4.5]; % km/s
-rho = [2.7 3.3]; % g/cm^3
-thik = [35 0];  % km
+%Earth model (ice/bedrock)
+vp = [3.87 5.8]; % km/s
+vs = [1.95 3.46]; % km/s
+rho = [0.917 2.72]; % g/cm^3
+% thik = [35 0];  % km
 nlyr = 2;
 
 %incident wave
@@ -42,8 +42,10 @@ m1 = dc_psv(...
     nt,v0,fs,...            % time samples of velocity-stress vector
     rayp);                  % ray parameter
 
+%% plot
 
 figure
-subplot(3,1,1); plot(t,vr,'k',t,vz,'r',t,rz,'b')
-subplot(3,1,2); plot(t,m1(2,:),t,m1(1,:))
-subplot(3,1,3); plot(t,m1(3,:),t,m1(4,:))
+subplot(3,1,1); plot(t,vr,'k',t,vz,'r',t,rz,'b'); legend('vr','vz','rz')
+subplot(3,1,2); plot(t,m1(1,:),t,m1(2,:)); legend('Pd','Pu')
+subplot(3,1,3); plot(t,m1(3,:),t,m1(4,:)); legend('Sd','Su')
+xlabel('Time (s)')
